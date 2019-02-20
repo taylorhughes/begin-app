4491dd3a28fbf212750f6c128ae52bb0a1ddfb94
Taylor Hughes <taylor@taylor-hughes.com>
2019-02-20 14:26:22 -0800

    Update bar

--- src/http/get-bar/index.js
+++ src/http/get-bar/index.js
@@ -8,16 +8,16 @@ let html = `
 <html lang=en>
   <head>
     <meta charset=utf-8>
-    <title>Hi!</title>
+    <title>Bar!</title>
     <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
     <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
   </head>
   <body>
     <h1 class="center-text">
-      Hello world!
+      Bar! Bar!
     </h1>
     <p class="center-text">
-      Your new route is ready to go!
+      Your new route is ready to go!!!!
     </p>
     <p class="center-text">
       Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
